e7b1972c6138ceab13faf50c515b21b52e3c4e25
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-09-09 08:41:07 +0200

    V-bind + const obj

--- src/pages/Simple.vue
+++ src/pages/Simple.vue
@@ -1,12 +1,18 @@
 <script setup lang="ts">
 import card from "../components/Card.vue";
 
+
+const obj = {
+    nom:"Faulkner Ave", prix:20, image:"/Mask Group3.jpg", nbrSDB:2
+}
+const obj2 = {nom:"Palm Harbor", prix:20, image:"/Mask Group2.png", nbrSDB:2}
+const obj3 = {nom:"Beverly Springfield", prix:20, image:"/Mask Group.png", nbrSDB:2}
 </script>
 <template>
     <h2>Page simple</h2>
     <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(345px,1fr))] gap-8 m-10 ">
-        <card nom="Beverly Springfield" prix="20" image="/Mask Group.png" nbrSDB="2" />
-        <card nom="Palm Harbor" prix="20" image="/Mask Group2.png" nbrSDB="2" />
-        <card nom="Faulkner Ave" prix="20" image="/Mask Group3.jpg" nbrSDB="2"/>
+        <card v-bind ="obj3" />
+        <card v-bind ="obj2" />
+        <card v-bind = "obj"/>
     </div>
 </template>
