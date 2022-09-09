e48225228505966cc857df5589edf1a58ec05b56
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-09-09 08:34:11 +0200

    image

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,29 +1,32 @@
 <script setup lang="ts">
 import bath from "./Icons/Bathrooms.vue"
-
+import heart from "./Icons/Heart.vue"
 defineProps( {
     nom: String,
     prix: Number,
     favori: Boolean,
     image: String, // les images sont simplement l'URL absolue (depuis la racine, débute par '/' )
     nbrSDB: Number,
+    size: String,
+    nbrSDB1 : Number,
+
   })
 </script>
 
 <template>
-  <figure>
-    <!-- Pour changer la valeur d'un attribut on utilise un "binding" :
+   <figure class="flex flex-col bg-white mx-4">
+     <!-- Pour changer la valeur d'un attribut on utilise un "binding" :
     https://vuejs.org/guide/essentials/template-syntax.html#attribute-bindings 
-    Ici on affecte à l'attribut 'src' la 'props' "image" -->
-    <img class="w-full h-48 object-cover" :src="image" alt="" />
+    Ici on affecte à l'attribut 'src' la 'props' "image"  -->
+    <img class="rounded-t-lg mt-5 w-full " :src="image" alt="" />
     <figcaption
       class="relative flex flex-col border-2 border-t-0 border-indigo-100"
     >
-      <!-- Pour changer une classe CSS en fonction d'un booléen : 
+     <!-- Pour changer une classe CSS en fonction d'un booléen : 
       https://vuejs.org/guide/essentials/class-and-style.html#binding-html-classes -->
       <heart :class="{ 'fill-red-300': favori }" />
 
-      <!-- Pour afficher du contenu textuel, simplement utiliser l'interpolation par double accolades (qui contiendront le JS dont l'interpretation donne le résultat affiché). -->
+      <!-- Pour afficher du contenu textuel, simplement utiliser l'interpolation par double accolades (qui contiendront le JS dont l'interpretation donne le résultat affiché).  -->
       <div class="text-2xl font-bold text-indigo-500 pr-1">${{ prix }}</div>
 
       <h3 class="text-2xl text-gray-900">{{ nom }}</h3>
@@ -33,4 +36,5 @@ defineProps( {
       <div><bath class="inline-block pr-1" />{{ nbrSDB }} Bathrooms</div>
     </figcaption>
   </figure>
+
 </template>
---
+++ src/pages/Simple.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import card from "../components/Card.vue";
+
+</script>
+<template>
+    <h2>Page simple</h2>
+    <div class="grid grid-flow-row-dense grid-cols-[repeat(auto-fit,minmax(345px,1fr))] gap-8 m-10 ">
+        <card nom="Beverly Springfield" prix="20" image="/Mask Group.png" nbrSDB="2" />
+        <card nom="Palm Harbor" prix="20" image="/Mask Group2.png" nbrSDB="2" />
+        <card nom="Faulkner Ave" prix="20" image="/Mask Group3.jpg" nbrSDB="2"/>
+    </div>
+</template>
