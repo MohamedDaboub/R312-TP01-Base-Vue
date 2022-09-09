c0fee7bcd00e52cd292897ec36eccf01c995647d
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-09-09 10:07:59 +0200

    fin

--- src/pages/Liste-fetch.vue
+++ src/pages/Liste-fetch.vue
@@ -1,8 +1,8 @@
-<script setup>
-    import Card from "../components/card.vue"
+<script setup lang="ts">
+    import Card from "../components/card.vue";
     const res = await fetch(maisons.json)
     const posts = await res.json()
 </script>
 <template>
-    <Card/>
+    <Card v-for="maison in maisons" :key="maison.nom" v-bind="maison"/>
 </template>
