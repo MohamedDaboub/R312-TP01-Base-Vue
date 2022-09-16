94af5653136deb1a7381eec24ec3f7549ee67e1b
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-09-16 11:00:01 +0200

    page Nbr

--- src/App.vue
+++ src/App.vue
@@ -27,6 +27,11 @@
           liste Fetch     
         </router-link>
       </li>
+      <li>
+        <router-link class="text-red-600 underline" to="/1">
+          Liste Nombre     
+        </router-link>
+      </li>
     </ul>
   </nav>
 
---
+++ src/pages/[nbr].vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+    import card from '../components/card.vue'
+    const res = await fetch("maisons.json")
+    const maisons = await res.json()
+    defineProps<{nbr:String}>();
+</script>
+
+    <template>
+        <div>
+            <card v-bind="maisons[Number(nbr)]"/>
+        </div>
+    </template>
