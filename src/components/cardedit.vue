0f2a29c2533159172debbd2647a5705eca870987
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-09-16 11:55:19 +0200

    TP02

--- src/components/cardedit.vue
+++ src/components/cardedit.vue
@@ -10,9 +10,15 @@
             <card v-bind="maison" />
         </div>
         <div class="p-2">
-            <FormKit type="form" v-model="maison" :submit-attrs="{ classes: { input: 'bg-blue-500 px-6   py-4 text-white flex my-4 centre     rounded' }}">
-                <FormKit name="nom" label="nom" />
-                <FormKit name="prix" label="prix" type="number"/>
+            <FormKit type="form" v-model="maison" :config="{
+                        classes: {
+                                    input: 'p-2 rounded border-gray-300 shadow-sm border bg-bleu-300 hover:border-2 hover:black',
+                                    label: 'text-black',
+                                },
+                    }"
+            :submit-attrs="{ classes: { input: 'bg-blue-900 px-6 justify-center   py-4 text-white flex my-4 centre rounded' }}">
+                <FormKit name="nom" label="Nom" wrapper-class="text-xl  " />
+                <FormKit name="prix" label="Prix" type="number" wrapper-class="text-xl  "/>
                 <FormKit name="favori" label="Mettre en valeur "
                 type="checkbox"  wrapper-class="flex text-xl   " class="" />
             </FormKit>
