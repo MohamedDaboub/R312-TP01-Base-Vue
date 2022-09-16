380848c30a7ba46367952cbfb64e23b3786611e3
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-09-16 11:02:45 +0200

    dossier Edit + page new

---
+++ src/pages/edit/new.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+    import { ref } from '@vue/reactivity'
+    import card from '@/components/card.vue'
+    const maison = ref({});
+    </script>
+    <template>
+        <div>
+            <div class="p-2">
+                <h2>Résultat (Prévisualisation)</h2>
+                <card v-bind="maison" />
+            </div>
+            <div class="p-2">
+                <Formkit type="form" v-model="maison">
+                    <Formkit name="nom" label="nom" />
+                    <Formkit name="prix" label="prix" type="number" />
+                    <Formkit name="favori" label="mettre en valeur "
+                    type="checkbox" />
+
+                </Formkit>
+
+            </div>
+        </div>
+    </template>
