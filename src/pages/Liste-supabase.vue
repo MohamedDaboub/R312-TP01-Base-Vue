b10e971e71bcad35395c8c8341e135d3776190f0
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-09-28 20:36:07 +0200

    installation supabase

---
+++ src/pages/Liste-supabase.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+    import Card from "../components/card.vue";
+    import {supabase} from "@/supabase";
+    // let {data: maisons, error} = await supabase.from("maisons").select("*")
+    console.log("Supabase:",supabase)
+    const maisons = []
+    // const {data: maisons, error} = await supabase.from("maisons").select("*")
+</script> 
+<template>
+    <div v-for="maison in maisons" :key="maison.nom">
+        <Card v-bind="maison"/>
+    </div>
+</template>
