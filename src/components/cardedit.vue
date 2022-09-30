8b68d036629526b07faf64ebff61e50f66d481e9
MohamedDaboub <mohamed.daboub@edu.univ-fcomte.fr>
2022-09-30 10:53:02 +0200

    TP03 fin

--- src/components/cardedit.vue
+++ src/components/cardedit.vue
@@ -1,8 +1,29 @@
 <script setup lang="ts">
+    import { useRouter } from "vue-router";
     import { ref } from '@vue/reactivity'
     import card from "./card.vue"
+    import {supabase} from "../supabase"
     const maison = ref({});
-    </script>
+    const router = useRouter();
+    async function upsertMaison(dataForm, node) {
+    const { data, error } = await supabase.from("Maison").upsert(dataForm);
+    if (error) node.setErrors([error.message])
+    else {
+    node.setErrors([]);
+    router.push({ name: "edit-id", params: { id: data[0].id } });
+    }   
+    }
+    const props = defineProps(["id"]);
+    if (props.id) {
+        // On charge les données de la maison
+        let { data, error } = await supabase
+        .from("Maison")
+        .select("*")
+        .eq("id", props.id);
+        if (error) console.log("n'a pas pu charger le table Maison :", error);
+        else maison.value = (data as any[])[0];
+}
+</script>
 <template>
     <div>
         <div class="p-2">
@@ -10,7 +31,7 @@
             <card v-bind="maison" />
         </div>
         <div class="p-2">
-            <FormKit type="form" v-model="maison" :config="{
+            <FormKit type="form" @submit="upsertMaison" v-model="maison" :config="{
                         classes: {
                                     input: 'p-2 rounded border-gray-300 shadow-sm border bg-bleu-300 hover:border-2 hover:black',
                                     label: 'text-black',
